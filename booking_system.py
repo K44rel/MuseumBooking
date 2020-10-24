f18ec538103ab38b5cafbf12503bbcc353c13cad
Kaarel Loide <kaarel.loide@gmail.com>
2020-10-24 19:29:11 +0300

    [Cycle 1, Step 1 - test failing] admin adds new personnel

--- booking_system.py
+++ booking_system.py
@@ -3,3 +3,22 @@ class BookingSystem:
     personnel = []
     appointment_types = []
     bookings = []
+
+    def add_new_personnel(self, personnel):
+        pass
+
+    def set_current_user(self, user):
+        pass
+
+
+class Personnel:
+    def __init__(self, personnel_type, name, availability_dates):
+        self.personnel_type = personnel_type
+        self.name = name
+        self.availability_dates = availability_dates
+
+
+class AvailabilityPeriod:
+    def __init__(self, from_date, to_date):
+        self.from_date = from_date
+        self.to_date = to_date
--- booking_system_test.py
+++ booking_system_test.py
@@ -1,5 +1,44 @@
 import unittest
+from booking_system import BookingSystem, Personnel, AvailabilityPeriod
+from datetime import datetime
 
 
 class TestBookingSystem(unittest.TestCase):
-    pass
+    """
+    Scenario: Administrator creates new personnel in the system
+
+        Administrators should be able to create new personnel.
+        Personnel shall be attached to appointment types.
+
+        Given the administrator is signed in to the management view
+        And the administrator navigates to the personnel creation view
+        When the administrator enters "teacher" for the type of the personnel
+        And the administrator enters "Joe" for the name of the personnel
+        And the administrator enters availability dates for the personnel
+        Then a new personnel is created in the system
+    """
+
+    def test_admin_creates_new_personnel(self):
+        system = BookingSystem()
+
+        # Current user has to be administrator
+        system.set_current_user("Administrator")
+
+        personnel = joe()
+
+        system.add_new_personnel(personnel)
+
+        # Check if new personnel was added
+        self.assertIn(personnel, system.personnel)
+
+
+def joe():
+    return Personnel(
+                "teacher", "Joe",
+                [AvailabilityPeriod(datetime(2020, 2, 2, 10), datetime(2020, 2, 2, 13)),
+                 AvailabilityPeriod(datetime(2020, 2, 4, 14), datetime(2020, 2, 4, 18))]
+            )
+
+
+if __name__ == '__main__':
+    unittest.main()
